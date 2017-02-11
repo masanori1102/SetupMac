8d60454fdf27c17c0b522f846835f99234a450b5
Masanori Ishigami <masanori.ishigami@gmail.com>
2017-02-12 02:04:55 +0900

    #3 Add script and setting file

---
+++ SetupMac.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This script is for installing the applications written in .
+
+echo "1. Install Homebrew"
+# "http://brew.sh/"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; echo
+
+echo "2. Install Homebrew Cask"
+# "https://caskroom.github.io/"
+brew install caskroom/cask/brew-cask ; echo
+
+if [ $? -ne 0 ] ; then
+    exit
+fi
+
+echo "3. Install applications with Homebrew Cask"
+cwd=`dirname "${0}"`
+cat ${cwd}/app.list | while read line
+do
+  if [ -z "${line}" ]; then
+    continue
+  fi
+
+  if `echo ${line} | grep -q "#"` ; then
+    continue
+  fi
+
+  echo ${line}
+  brew cask install ${line}
+done
+
+echo "===== END ===="
